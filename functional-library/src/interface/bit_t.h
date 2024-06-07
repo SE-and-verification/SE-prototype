5fa6c3c42419c37197c79e0d83a14bfe42b2572a
Yishen Zhou <yishenzh@umich.edu>
2024-06-06 22:43:35 -0400

    Implemented and modified some basic bit316_t class functions.

--- functional-library/src/interface/bit_t.h
+++ functional-library/src/interface/bit_t.h
@@ -3,6 +3,7 @@
 #include <stdlib.h> 
 #include <bitset>
 #include <cstdint>
+#include <vector>
 #include <string.h>
 #include <iostream>
 
@@ -311,55 +312,28 @@ public:
 
 // } bit64_t;
 
-typedef struct bit316_t{
+class bit316_t {
 
-public: //SHOULD BE PRIVATE
+private:
     uint8_t value[40]; 
     // The bit316_t class stores values according to the following structure:
     // value[0]              : 4 dummy bits, should be 0       <- MSB
     // value[1]  - value[7]  : 60-bit hash 
     // value[8]  - value[23] : 128-bit upper half cypher_text
     // value[24] - value[39] : 128-bit lower half cypher_text  <- LSB
-
-private:
-    // char bit_str[129]; //128 bit-characters, followed by '\0'
-    // char hex_str[33];  //16  hex-characters, followed by '\0'
-
+    
+    char bit_str[321]; // 4 dummy bits and 316 bit-characters, followed by '\0'
+    char hex_str[81]; // 4 dummy bits and 79 hex-characters, followed by '\0'
 
 public:
-    /******** CONSTRUCTORS ********/
+    // ***Constructors***
     bit316_t() {
-        for(int i = 0; i < 39; i++) {
-            value[i] = 0x00; //0000 0000
+        for(int i = 0; i < 40; i++) {
+            value[i] = 0x00;
         } 
     }
-    bit316_t(__uint128_t ciphtext_upper, __uint128_t ciphtext_lower, uint64_t hash) {
-        init(ciphtext_upper, ciphtext_lower, hash);
-    }
-    // bit128_t(uint8_t upper_value, uint8_t* lower_pad){
-    //     /* Store upper_value in MSB/Index 0 */
-    //     value[0]= upper_value;
-    //     /* Store lower_pad in 8-bit segments */
-    //     for(int i=15; i>=1 ; i--){
-    //         value[i] = lower_pad[i-1];
-    //     } 
-    // }
-    // bit128_t(uint8_t* full_value){
-    //     /* Store full_value in 8-bit segments */
-    //     for(int i=0; i<16; i++){
-    //         value[i] = full_value[i];
-    //     } 
-    // }
-    // bit128_t(__uint128_t full_value){
-    //     /* Store full_value in 8-bit segments */
-    //     memcpy(this->value, &full_value, sizeof(this->value));
-    // }
-    // __uint128_t convert_to_128(){
-    //     __uint128_t result = 0;
-    //     memcpy(&result, this->value, sizeof(this->value));
-    //     return result;
-    // }
-    void init(__uint128_t ciphtext_upper, __uint128_t ciphtext_lower, uint64_t hash){
+
+    bit316_t(__uint128_t ciphtext_lower, __uint128_t ciphtext_upper, uint64_t hash) {
         // Store lower half into LS 128 bits
         for(int i = 39; i >= 24; i--) {
             value[i] = (ciphtext_lower) & 0xFF;
@@ -371,15 +345,27 @@ public:
             ciphtext_upper = ciphtext_upper >> 8;
         }
         // Store hash into MS 60 (64) bits
-        for(int i = 7; i >= 1; i--) {
+        for(int i = 7; i >= 0; i--) {
             value[i] = (hash) & 0xFF;
             hash = hash >> 8;
         }
-        value[0] = 0;
     }
 
-    /******** VALUE ACCESS FUNCTIONS ********/
-    __uint128_t getLowerCiph_128b(){
+    bit316_t(const uint8_t* full_value) {
+        // Convert from array 
+        for(int i = 0; i < 40; i++) {
+            value[i] = full_value[i];
+        } 
+    }
+    bit316_t(const std::vector<uint8_t> &full_value) {
+        // Convert from vector
+        for(int i = 0; i < 40; i++) {
+            value[i] = full_value[i];
+        } 
+    }
+
+    // ***Value Access***
+    __uint128_t getLowerCiph_128b() {
         __uint128_t lower_128_bits = 0;
         for (int i = 0; i < 16; i++) {
             lower_128_bits = (lower_128_bits << 8) | value[24 + i];
@@ -395,52 +381,41 @@ public:
         return upper_128_bits;        
     }
 
-    uint64_t getHashValue_64b(){
+    uint64_t getHashValue_64b() {
         __uint128_t hash_value = 0;
-        for (int i = 0; i < 16; i++) {
-            hash_value = (hash_value << 8) | value[1 + i];
+        for (int i = 0; i < 8; i++) {
+            hash_value = (hash_value << 8) | value[i];
         }
         return hash_value; 
     }
 
+    // ***Print Utilities*** 
+    char* get_bit_cstr() {
+        // Update and return bit_str
+        for (int i = 0; i < 40; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                bit_str[i * 8 + j] = (value[i] & (1 << (7 - j))) ? '1' : '0';
+            }
+        }
+        bit_str[320] = '\0';
+        return bit_str;
+    }
 
-    // /******** PRINT UTILITIES ********/
-    // char* get_bit_cstr(){
-    //     char* dest = bit_str; 
-    //     for(int i=0; i<16; i++){
-    //         // Convert uint8_t to bits
-    //         std::bitset<8> x(value[i]); 
-    //         const char* mystr = x.to_string().c_str();
-    //         // Copy bits into destination
-    //         strncpy(dest, mystr, 8);
-    //         dest += 8;
-    //     }
-    //     bit_str[128] = '\0';
-    //     return bit_str;
-    // }
+    char* get_hex_cstr() {
+        // Update and return hex_str
+        for (int i = 0; i < 40; ++i) {
+            sprintf(hex_str + i * 2, "%02x", value[i]);
+        }
+        hex_str[80] = '\0';
+        return hex_str;
+    }
 
-    // char* get_hex_cstr(){
-    //     uint8_t* values = value; 
-    //     int val_len = 16;
-    //     char* dest = hex_str; 
-    //     while(val_len--){
-    //         // Convert uint8_t into hex and copy into destination
-    //         sprintf(dest, "%02x", *values);
-    //         dest += 2;
-    //         ++values;
-    //     }
-    //     hex_str[32] = '\0';
-    //     return hex_str;
-    // }
-
-    // void print_hex(){
-    //     char* to_print = get_hex_cstr();
-    //     printf("\t0x");
-    //     for(int i=0; i<32; i+=2){
-    //         printf(" %c%c", to_print[i], to_print[i+1]);
-    //     }
-    //     printf("\n");
-    // }
+    void print_hex() {
+        // Output hex_str
+        char* to_print = get_hex_cstr();
+        printf("\t0x");
+        puts(to_print);
+    }
 
     // void print_hex_parted_32(){
     //     char* to_print = get_hex_cstr();
@@ -468,18 +443,18 @@ public:
     // }
 
 
-    // void print_bin(){
-    //     char* to_print = get_bit_cstr();
-    //     printf("\t  ");
-    //     for(int i=0; i<16; i++){
-    //         printf(" ");
-    //         for(int j=0; j<8; j++){
-    //             // Lazy about typing, so doing a double loop here :) 
-    //             printf("%c", to_print[(8*i) + j]);
-    //         }
-    //     }
-    //     printf("\n");
-    // }
+    void print_bin(){
+        // Output bit_str
+        char* to_print = get_bit_cstr();
+        printf("\t0b");
+        for(int i = 0; i < 40; i++) {
+            printf(" ");
+            for(int j = 0; j < 8; j++) {
+                printf("%c", to_print[8 * i + j]);
+            }
+        }
+        printf("\n");
+    }
 
     // void print_bin_parted_32(){
     //     char* to_print = get_bit_cstr();
@@ -535,7 +510,6 @@ public:
     //     }
     // }
 
-} bit316_t;
-
+};
 
 #endif
