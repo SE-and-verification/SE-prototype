a54025004f44325a2283e962c19257653c9ad386
Yishen Zhou <yishenzh@umich.edu>
2024-06-12 22:30:46 -0400

    Fixed -Wreturn-type warning in interface.h file.

--- functional-library/src/interface/interface.h
+++ functional-library/src/interface/interface.h
@@ -6,37 +6,48 @@
 
 
 
-bit128_t test_encrypt_128_128(uint8_t* full_value){
-
+bit128_t test_encrypt_128_128(uint8_t* full_value) {
     bit128_t plaintext(full_value);
-
-    switch(CIPHER){
-        case AES128:    return aes128_encrypt_128(plaintext);
+    switch(CIPHER) {
+        case AES128:    
+            return aes128_encrypt_128(plaintext);
+        default:
+            std::cerr << "Invalid CIPHER!\n";
+            bit128_t dummy; 
+            return dummy;
     }
 }
 
-uint64_t test_qarma_encrypt_128_128(uint64_t* value){
+uint64_t test_qarma_encrypt_128_128(uint64_t* value) {
     // uint64_t pad = get_random_pad_64b();
     uint64_t pad = 0;
     // uint64_t pad = 0x313198a2e0370734;    //Pad for AES TEST
     // uint64_t pad = 0x6572732064657363;      //Pad for Simon Test
     bit128_t plaintext(*value, pad);
     // printf("PLA:"); plaintext.print_hex();
-
-    switch(CIPHER){
-        case AES128:    return aes128_encrypt_128(plaintext).getUpperValue_64b();
+    switch(CIPHER) {
+        case AES128:    
+            return aes128_encrypt_128(plaintext).getUpperValue_64b();
+        default:
+            std::cerr << "Invalid CIPHER!\n";
+            return (uint64_t) 0;
     }
 
 }
 
 
-bit128_t test_decrypt_128_128(bit128_t ciphertext){
-    switch(CIPHER){
-        case AES128:    return aes128_decrypt_128(ciphertext);
+bit128_t test_decrypt_128_128(bit128_t ciphertext) {
+    switch(CIPHER) {
+        case AES128:    
+            return aes128_decrypt_128(ciphertext);
+        default:
+            std::cerr << "Invalid CIPHER!\n";
+            bit128_t dummy; 
+            return dummy;
     }
 }
 
-bit128_t encrypt_64_128(uint64_t* value){
+bit128_t encrypt_64_128(uint64_t* value) {
     // uint64_t pad = get_mersenne_random_pad_64b();
     uint64_t pad = 0;
     // uint64_t pad = 0x313198a2e0370734;    //Pad for AES TEST
@@ -44,21 +55,28 @@ bit128_t encrypt_64_128(uint64_t* value){
     bit128_t plaintext(*value, pad);
     // printf("PLA:"); plaintext.print_hex();
 
-    switch(CIPHER){
-        case AES128:    {
+    switch(CIPHER) {
+        case AES128:
             return aes128_encrypt_128(plaintext);
-            }
+        default:
+            std::cerr << "Invalid CIPHER!\n";
+            bit128_t dummy; 
+            return dummy;
     }
 }
 
-uint64_t decrypt_128_64(bit128_t ciphertext){
-    switch(CIPHER){
-        case AES128:    return aes128_decrypt_128(ciphertext).getUpperValue_64b();
+uint64_t decrypt_128_64(bit128_t ciphertext) {
+    switch(CIPHER) {
+        case AES128:    
+            return aes128_decrypt_128(ciphertext).getUpperValue_64b();
+        default:
+            std::cerr << "Invalid CIPHER!\n";
+            return (uint64_t) 0;
     }
 }
 
 
-bit128_t encrypt_8_128(uint8_t* value){
+bit128_t encrypt_8_128(uint8_t* value) {
     uint8_t pad[15];
     for(int i=0; i<15; i++){
         // pad[i] = get_mersenne_random_pad_8b();
@@ -67,14 +85,23 @@ bit128_t encrypt_8_128(uint8_t* value){
     bit128_t plaintext(*value, pad);
     // printf("PLA:"); plaintext.print_hex();
 
-    switch(CIPHER){
-        case AES128:    return aes128_encrypt_128(plaintext);
+    switch(CIPHER) {
+        case AES128:    
+            return aes128_encrypt_128(plaintext);
+        default:
+            std::cerr << "Invalid CIPHER!\n";
+            bit128_t dummy; 
+            return dummy;
     }
 }
 
-uint8_t decrypt_128_8(bit128_t ciphertext){
-    switch(CIPHER){
-        case AES128:    return aes128_decrypt_128(ciphertext).getUpperValue_8b();
+uint8_t decrypt_128_8(bit128_t ciphertext) {
+    switch(CIPHER) {
+        case AES128:    
+            return aes128_decrypt_128(ciphertext).getUpperValue_8b();
+        default:
+            std::cerr << "Invalid CIPHER!\n";
+            return (uint8_t) 0;
     }
 }
 
