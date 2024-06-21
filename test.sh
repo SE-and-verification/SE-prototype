a5210031aa09852d978397fc427095d9484ab53d
Duo Xu <xuduo@umich.edu>
2024-06-20 23:43:15 -0400

    add bash script for testing SE

---
+++ test.sh
@@ -0,0 +1,9 @@
+cd ~/SE-prototype
+make
+cd ~/SE-prototype/functional-library/src/ciphers/aes/tinyCrypt_without_Git
+make
+cd ~/SE-prototype/functional-library/src/rng/mersenne-twister-1
+make
+cd ~/SE-prototype/functional-library/src/verilator
+make
+./test
