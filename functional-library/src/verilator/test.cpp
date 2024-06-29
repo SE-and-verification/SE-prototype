59cd5b13954004c57ba7bed5784b123007500283
Yishen Zhou <yishenzh@umich.edu>
2024-06-29 01:43:11 -0400

    Modified test.cpp: print more intermediate value. Bugs found and located.

--- functional-library/src/verilator/test.cpp
+++ functional-library/src/verilator/test.cpp
@@ -9,43 +9,29 @@
 #include "verilated.h"
 #include "Instruction.h"
 #include "enc_lib.h"
- 
-int print128(__uint128_t print_item){
-	uint64_t lower = (uint64_t)print_item;
-	uint64_t upper = (uint64_t)(print_item>>64);
-	std::cout<< std::setw(16) << std::setfill('0') << std::hex << upper<<"|"<<lower<<std::endl;
-	return 0;
-}
-int print128dec(__uint128_t dec){
-	__uint128_t print_item = aes128_decrypt_128(bit128_t(dec)).convert_to_128();
-	uint64_t lower = (uint64_t)print_item;
-	uint64_t upper = (uint64_t)(print_item>>64);
-	std::cout <<"decrypted: " << std::setw(16) << std::setfill('0') <<std::hex << upper<<"|"<<lower<<std::endl;
-	return 0;
-}
 
-int print316(bit316_t dec) {
+int print316(bit316_t &dec) {
 	// Get the original value to print
-	__uint128_t print_item_upper 	= dec.getUpperCiph_128b();
-	__uint128_t print_item_lower 	= dec.getLowerCiph_128b();
+	__uint128_t print_item_upper 	= aes128_decrypt_128(bit128_t(dec.getUpperCiph_128b())).convert_to_128();
+	__uint128_t print_item_lower 	= aes128_encrypt_128(aes128_decrypt_128(bit128_t(dec.getLowerCiph_128b()))).convert_to_128();
 	uint64_t print_hash 			= dec.getHashValue_64b();
-	
+
 	// Split them into uint64_t type values
-	uint64_t upper_high = (uint64_t) (print_item_upper >> 64);
-    uint64_t upper_low  = (uint64_t) (print_item_upper & 0xFFFFFFFFFFFFFFFF);
-	uint64_t lower_high = (uint64_t) (print_item_lower >> 64);
-    uint64_t lower_low  = (uint64_t) (print_item_lower & 0xFFFFFFFFFFFFFFFF);
+	uint64_t upper_high = (uint64_t)(print_item_upper >> 64);
+    uint64_t upper_low = (uint64_t)(print_item_upper & 0xFFFFFFFFFFFFFFFF);
+	uint64_t lower_high = (uint64_t)(print_item_lower >> 64);
+    uint64_t lower_low = (uint64_t)(print_item_lower & 0xFFFFFFFFFFFFFFFF);
 
 	// Print the values
-	printf("Ciphertext:\n");
+	printf("Plaintext:\n");
 	printf("\tUpper 128 bits: %016" PRIx64 " %016" PRIx64 "\n", upper_high, upper_low);
     printf("\tLower 128 bits: %016" PRIx64 " %016" PRIx64 "\n", lower_high, lower_low);
 	printf("\tHash: %016" PRIx64 "\n", print_hash);
-	
+
 	return 0;
 }
 
-int print316dec(bit316_t dec) {
+int print316dec(bit316_t &dec) {
 	// Get the original value to print
 	__uint128_t print_item_upper 	= aes128_decrypt_128(bit128_t(dec.getUpperCiph_128b())).convert_to_128();
 	__uint128_t print_item_lower 	= aes128_decrypt_128(bit128_t(dec.getLowerCiph_128b())).convert_to_128();
@@ -74,20 +60,34 @@ int print316dec(bit316_t dec) {
 // }
 
 int main() {
-	printf("----------TESTING----------\n");
+	printf("----------TESTING----------\n\n");
+
+	// setting parameters
 	printf("Begin setting parameters.\n");
 	setParameters();
 	printf("Finish setting parameters.\n---\n");
+	
+	// generate SE simulator
 	printf("Begin generating SE simulator.\n");
 	SE se_simulator;
 	printf("Finish generating SE simulator.\n---\n");
-	printf("Begin generating enc_int l1, l2 and l3.\n");
+	
+	// generate l1, l2 (opA and opB)
+	printf("Begin generating enc_int for ALU computation part (lower 128 bits).\n\n");
 	enc_lib::enc_int l1 = 1;
 	enc_lib::enc_int l2 = 2;
-	enc_lib::enc_int l3 = l1 + l2;
-	// enc_lib::enc_int l6 = l2 * l3;
-	printf("Finish generating enc_int l1, l2 and l3.\n---\n");
-	printf("Begin calculating initial cyphertext.\n");
+	printf("\t(l1) Ciphertext_A (lower 128 bits): ");
+	l1.ciphertext.print_hex();
+	printf("\t(l1) Plaintext_A (lower 128 bits): ");
+	printf("%x\n", l1.GET_DECRYPTED_VALUE());
+	printf("\t(l2) Ciphertext_B (lower 128 bits): ");
+	l2.ciphertext.print_hex();
+	printf("\t(l2) Plaintext_B (lower 128 bits): ");
+	printf("%x\n\n", l2.GET_DECRYPTED_VALUE());
+	printf("Finish generating enc_int.\n---\n");
+	
+	// generate upper 128 bits
+	printf("Begin generating initial cyphertext for comparison part (upper 128 bits).\n\n");
 	// Generating plaintext array
 	// Structure: [rtsni'][Y_hsh][X_hsh] (old)
 	uint8_t plaintext_A[16] = {0x2c, 0x1c, 0xa7, 0x76, 0xab, 0x19, 0x4b, 0x70, 0x3e, 0xee, 0xf2, 0x9a, 0x45, 0xfa, 0x99, 0x99};
@@ -100,49 +100,29 @@ int main() {
 	// software ENCrypt
 	bit128_t init_A = aes128_encrypt_128(plaintext_A_bit128t);
 	bit128_t init_B = aes128_encrypt_128(plaintext_B_bit128t);
-	printf("\tciphtext_A: ");
+	printf("\t(l1) plaintext_A (upper 128 bits): ");
 	for(int i = 0; i < 16; ++i) {
-        printf("%02x", init_A.value[i]);
+        printf("%02x ", plaintext_A_bit128t.value[i]);
     }
 	printf("\n");
-	printf("\tciphtext_B: ");
+	printf("\t(l1) ciphtext_A (upper 128 bits): ");
 	for(int i = 0; i < 16; ++i) {
-        printf("%02x", init_B.value[i]);
+        printf("%02x ", init_A.value[i]);
     }
 	printf("\n");
-	printf("Finish calculating initial cyphertext.\n---\n");
-	printf("Begin software decryption self checking.\n");
-	// software DECrypt and compare
-	bit128_t test_A = aes128_decrypt_128(init_A);
-	bit128_t test_B = aes128_decrypt_128(init_B);
-	printf("\tplaintext_A: ");
+	printf("\t(l2) plaintext_B (upper 128 bits): ");
 	for(int i = 0; i < 16; ++i) {
-        printf("%02x", plaintext_A_bit128t.value[i]);
+        printf("%02x ", plaintext_B_bit128t.value[i]);
     }
 	printf("\n");
-	printf("\tsoftwareDeA: ");
+	printf("\t(l2) ciphtext_B (upper 128 bits): ");
 	for(int i = 0; i < 16; ++i) {
-        printf("%02x", test_A.value[i]);
+        printf("%02x ", init_B.value[i]);
     }
-	printf("\n");
-	printf("\tplaintext_B: ");
-	for(int i = 0; i < 16; ++i) {
-        printf("%02x", plaintext_B_bit128t.value[i]);
-    }
-	printf("\n");
-	printf("\tsoftwareDeB: ");
-	for(int i = 0; i < 16; ++i) {
-        printf("%02x", test_B.value[i]);
-    }
-	printf("\n");
-	for(int i = 0; i < 16; ++i) {
-        if((plaintext_A_bit128t.value[i] != test_A.value[i]) || (plaintext_B_bit128t.value[i] != test_B.value[i])) {
-			printf("Finish software decryption self checking. Error found!\n");
-			exit(1);
-		}
-    }
-	printf("Finish software decryption self checking.\n---\n");
-	printf("Begin instantiating bit316_t opA and opB.\n");
+	printf("\n\n");
+	printf("Finish generating initial cyphertext for comparison part (upper 128 bits).\n---\n");
+	
+	printf("Begin generating bit316_t opA and opB.\n\n");
 	__uint128_t init_A_uint128 = 0;
 	__uint128_t init_B_uint128 = 0;
     for(int i = 0; i < 16; ++i) {
@@ -151,32 +131,82 @@ int main() {
     }
 	bit316_t opA(l1.ciphertext.convert_to_128(), init_A_uint128, (uint64_t) (init_A_uint128 >> 68));
 	bit316_t opB(l2.ciphertext.convert_to_128(), init_B_uint128, (uint64_t) (init_B_uint128 >> 68));
-	printf("Finish instantiating bit316_t opA and opB.\n---\n");
-	// __uint128_t l3_veri = SE::SECompute(l1.ciphertext.convert_to_128(), l2.ciphertext.convert_to_128(), 0, Instruction::ADD())
-
-	// printf("%d\n",decrypt_128_64( l3_veri));
-	// printf("%d:\n",1);
-	// print128(l1.ciphertext.convert_to_128());
-	// printf("%d:\n",2);
-	// print128(l2.ciphertext.convert_to_128());
-	// print128dec(l2.ciphertext.convert_to_128());
-	// print128dec(l1.ciphertext.convert_to_128());
-	// print128dec(l3.ciphertext.convert_to_128());
-	// print128(l3.ciphertext.convert_to_128());
-	// printf("ticks: %d\n", SE::real_tickcount);
-
-	printf("Begin SECompute.\n");
+	uint8_t* ptr_A = opA.get_value();
+	uint8_t* ptr_B = opB.get_value();
+	printf("\t(l1) opA input:\n");
+	printf("\thash: ");
+	for(int i = 0; i < 8; ++i) {
+        printf("%02x ", ptr_A[i]);
+    }
+	printf("\n\tupper 128 bits: ");
+	for(int i = 0; i < 16; ++i) {
+        printf("%02x ", ptr_A[i + 8]);
+    }
+	printf("\n\tlower 128 bits: ");
+	for(int i = 0; i < 16; ++i) {
+        printf("%02x ", ptr_A[i + 24]);
+    }
+	printf("\n");
+	printf("\t(l2) opB input:\n");
+	printf("\thash: ");
+	for(int i = 0; i < 8; ++i) {
+        printf("%02x ", ptr_B[i]);
+    }
+	printf("\n\tupper 128 bits: ");
+	for(int i = 0; i < 16; ++i) {
+        printf("%02x ", ptr_B[i + 8]);
+    }
+	printf("\n\tlower 128 bits: ");
+	for(int i = 0; i < 16; ++i) {
+        printf("%02x ", ptr_B[i + 24]);
+    }
+	printf("\n\n");
+	printf("Finish generating bit316_t opA and opB.\n---\n");
+	
+	printf("Begin SECompute.\n\n");
 	bit316_t l3_SE = SE::SECompute(opA, opB, 0, Instruction::ADD());
-	printf("Finish SECompute.\n---\n");
-	printf("Begin comparison.\n");
-	print316(l3_SE);
-	print316dec(l3_SE);
-	if(l3.ciphertext.convert_to_128() == l3_SE.getLowerCiph_128b()) {
-		printf("\tThe computation part is correct!\n");
-	} else {
-		printf("\tThe computation part is incorrect!\n");
-	}
-	printf("Finish comparison.\n");
+	uint8_t* ptr_C = l3_SE.get_value();
+	printf("\n\t(l3 computation) output (Ciphertext):\n");
+	printf("\thash: ");
+	for(int i = 0; i < 8; ++i) {
+        printf("%02x ", ptr_C[i]);
+    }
+	printf("\n\tupper 128 bits: ");
+	for(int i = 0; i < 16; ++i) {
+        printf("%02x ", ptr_C[i + 8]);
+    }
+	printf("\n\tlower 128 bits: ");
+	for(int i = 0; i < 16; ++i) {
+        printf("%02x ", ptr_C[i + 24]);
+    }
+	printf("\n");
+	printf("\nFinish SECompute.\n---\n");
+
+	printf("Begin Hash comparison part (upper 128 bits) check.\n\n");
+	// Print software-generated value
+	printf("> Software-generated value:\n\n");
+	// TODO:
+
+	// Print hardware-generated value
+	printf("> Hardware-generated value:\n\n");
+	// TODO:
+
+	printf("Finish Hash comparison part (upper 128 bits) check.\n---\n");
+	
+	printf("Begin ALU computation part (lower 128 bits) check.\n\n");
+	enc_lib::enc_int l3 = l1 + l2;
+	// Print software-generated value
+	printf("> Software-generated value:\n\n");
+	printf("\tCiphertext of result (lower 128 bits): ");
+	l3.ciphertext.print_hex();
+	printf("\tPlaintext of result (lower 128 bits): ");
+	printf("%x\n\n", l3.GET_DECRYPTED_VALUE());
+
+	// Print hardware-generated value
+	printf("> Hardware-generated value:\n\n");
+	// TODO:
+	printf("Finish ALU computation part (lower 128 bits) check.\n\n");
+
 	printf("------------END------------\n");
 
 	// // print128(l6.ciphertext.convert_to_128());
